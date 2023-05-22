b34b4cdb2b0a47fdbbb95ee0025c52276efe5e23
ram1117 <ramkumark1@outlook.com>
2023-05-22 22:30:20 +0530

    add views, model and controller for recipes

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,13 @@
+class RecipesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index,:show] do
+    resources :recipes, only: [:index, :create, :show, :destroy ]
+  end
 end
