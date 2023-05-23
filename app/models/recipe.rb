ce3fa7a643b33f57388d63f5935e4ba166549082
Ram Kumar Karuppusamy <ramkumark1@outlook.com>
2023-05-23 19:15:13 +0530

    Revert "Add recipe list"

--- app/models/recipe.rb
+++
@@ -1,7 +0,0 @@
-class Recipe < ApplicationRecord
-  belongs_to :user
-  validates :name, presence: true, length: { maximum: 250 }
-  validates :description, presence: true
-  validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
-  validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
-end
