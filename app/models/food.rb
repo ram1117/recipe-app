3c3694b117644408bb4ebdeedcb20ee1420f23bf
ram1117 <ramkumark1@outlook.com>
2023-05-24 10:12:59 +0530

    add many-to-many association using receipe_food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipe_food, dependent: :destroy
+  has_many :recipe, through: :recipe_food
 
   validates :name, presence: true
   validates :measurement_unit, presence: true
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_food, dependent: :destroy
+  has_many :food, through: :recipe_food
+
   validates :name, presence: true, length: { maximum: 250 }
   validates :description, presence: true
   validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
