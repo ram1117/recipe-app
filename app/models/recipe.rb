b0d23d1c44f204a479a36230d819983b004163ab
ram1117 <ramkumark1@outlook.com>
2023-05-25 13:08:47 +0530

    add cancanca and abilities to recipe and recipe foods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,8 @@ class Recipe < ApplicationRecord
   validates :description, presence: true
   validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
   validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
+
+  def owner?(user)
+    self.user == user
+  end
 end
