320e3604506d2e03c432f53aef5f75565541c054
Lancelot-SO <fsowah001@gmail.com>
2023-05-26 10:07:49 +0000

    add integration test

--- spec/features/food_spec.rb
+++ spec/features/food_spec.rb
@@ -4,15 +4,27 @@ RSpec.describe 'Foods', type: :feature do
   before(:each) do
     @user = User.create(name: 'Test user', email: 'test444@gmail.com', password: '123456',
                         password_confirmation: '123456', confirmation_token: nil, confirmed_at: Time.now)
-    @recipe = Recipe.create(name: 'Test recipe', preparation_time: 10.2, cooking_time: 20.3,
-                            description: 'Test description', public: true, user_id: @user.id)
-    @food = Food.create(name: 'Test food', price: 12.2, quantity: 4, measurement_unit: 'gram', user_id: @user.id)
-    @food_two = Food.create(name: 'Test food Two', price: 12.2, quantity: 4, measurement_unit: 'gram',
-                            user_id: @user.id)
-    @recipe_food = RecipeFood.create(quantity: 10, recipe_id: @recipe.id, food_id: @food.id)
+    @food = Food.create(name: 'Pizza', price: 5, measurement_unit: 'gram', quantity: 4, user_id: @user.id)
+    # post user_session_path params: { user: { email: @user.email, password: @user.password } }
+
     visit new_user_session_path
+
     fill_in 'Email', with: @user.email
     fill_in 'Password', with: @user.password
     click_button 'Log in'
+  end
+  describe 'food new page' do
+    scenario 'should display add food page' do
+      visit new_user_food_path(user_id: @user.id)
+      expect(page).to have_content('Add a new food')
+    end
+    scenario 'should have a button to add food' do
+      visit user_foods_path(user_id: @user.id)
+      expect(page).to have_link('Add food')
+    end
+    scenario 'should have a button to delete food' do
+      visit user_foods_path(user_id: @user.id)
+      expect(page).to have_content('Delete')
+    end
   end
 end
