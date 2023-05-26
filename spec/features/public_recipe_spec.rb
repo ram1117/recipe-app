285a1eec539e1c4f72675a9590fc230221042262
Lancelot-SO <fsowah001@gmail.com>
2023-05-26 10:34:32 +0000

    fix linters error

--- spec/features/public_recipe_spec.rb
+++ spec/features/public_recipe_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'PublicRecipes', type: :feature do
   before(:each) do
     @user = User.create(name: 'Test user', email: 'test444@gmail.com', password: '123456',
-    password_confirmation: '123456', confirmation_token: nil, confirmed_at: Time.now)
+                        password_confirmation: '123456', confirmation_token: nil, confirmed_at: Time.now)
     @recipe = Recipe.create(name: 'Test recipe', preparation_time: 10.2, cooking_time: 20.3,
                             description: 'Test description', public: true, user_id: @user.id)
 
